b827765f315788d60010dcca8d063a5eb9493ac0
Juxtopposed <juxtopposed.me@gmail.com>
2024-04-17 21:23:36 +0400

    add all

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const textElement = document.querySelector(".animated-text")
+const texts = [
+    "Everything is going too fast",
+    "But you’re okay",
+    "You are enough",
+    "Re-assure yourself",
+    "Click the button and be free",
+    "No matter how hard things are",
+    "The tools are there for you",
+    "You need to chillax",
+    "Everything is gonna be fine",
+    "Just go with the flow",
+    "Plan your way through",
+    "You’ve got this",
+    "Things might be overwhelming rn",
+];
+
+let index = 0;
+
+textElement.classList.add('opacity-100');
+
+setInterval(() => {
+    textElement.classList.remove('opacity-100');
+    setTimeout(() => {
+        textElement.textContent = texts[index];
+        index = (index + 1) % texts.length;
+        textElement.classList.add('opacity-100');
+    }, 300);
+}, 3000);
+
+
+function addPoint(element) {
+    const spanElement = element.querySelector('.point-span');
+
+    spanElement.classList.add('animate-point');
+
+    setTimeout(() => {
+        spanElement.classList.remove('animate-point');
+    }, 1500);
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'text': '#e5e5e5',
+      'background': '#0B0F14',
+      'primary': '#FF3495',
+      'secondary': '#87B0FF',
+      'accent': '#FFAE34',
+      'green': '#16a34a',
+    },
+    extend: {
+      fontSize: {
+        sm: '0.707rem',
+        base: '1rem',
+        xl: '1.414rem',
+        '2xl': '1.999rem',
+        '3xl': '2.827rem',
+        '4xl': '3.997rem',
+        '5xl': '5.652rem',
+      },
+      fontFamily: {
+        heading: 'Manrope',
+        body: 'Manrope',
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+        bold: '800',
+      },
+      blur: {
+        '5xl': '180px',
+      },
+      animation: {
+        'bloba': 'bloba 10s infinite ease-in-out',
+        'blobb': 'blobb 10s infinite ease-in-out',
+        'point': 'point 1.5s ease-out forwards',
+      },
+      keyframes: {
+        'bloba': {
+          '0%': { transform: 'translate(10rem, -6rem)', 'background-color': 'var(--secondary)'},
+          '50%': { transform: 'translate(-10rem, 6rem)', 'background-color': 'var(--primary)'},
+          '100%': { transform: 'translate(10rem, -6rem)', 'background-color': 'var(--secondary)'},
+        },
+        'blobb': {
+          '0%': { transform: 'translate(-10rem, 6rem)', 'background-color': 'var(--secondary)'},
+          '50%': { transform: 'translate(10rem, -6rem)', 'background-color': 'var(--accent)'},
+          '100%': { transform: 'translate(-10rem, 6rem)', 'background-color': 'var(--secondary)'},
+        },
+        'point': {
+          '0%': { transform: 'translate(-50%, 0)', opacity: '0' },
+          '1%': { transform: 'translate(-50%, 0)', opacity: '100%' },
+          '99%': { transform: 'translate(-50%, -500%)', opacity: '0' },
+          '100%': { transform: 'translate(-50%, 0)', opacity: '0' },
+        },
+      }
+    },
+  },
+  plugins: [],
+}
